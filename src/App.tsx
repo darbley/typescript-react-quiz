a946103552b65d057d738c6f24891534b5dac790
Brad Stapleton <brad@rethinkcanada.com>
2024-03-21 15:56:23 -0400

    add FullPageLoader and some logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@
 import './App.scss'
 import Score from './components/Score/Score.tsx';
 import Game from './components/Game/Game.tsx';
+import FullPageLoader from './components/FullPageLoader/FullPageLoader';
 import { useQuiz } from './context/QuizContext/QuizContext.tsx';
 import { useEffect } from 'react';
 import { Question, QuestionResponse } from './context/QuizContext/QuizContext.tsx';
+
+
 function App() {
 
   const { quizState, dispatch } = useQuiz();
 
   const fetchQuestions = async () => {
-    dispatch({ type: 'setStatus', payload: 'fetching'})
-    const response = await fetch('https://opentdb.com/api.php?amount=10')
-    // Add Type Annotation for the data
-    const data : QuestionResponse = await(response.json())
-    console.log('data is ',data);
-    if(data.response_code === 0){
-      let question : Question = data.results[0].question
-      dispatch({ type: 'setStatus', payload: 'ready'})
-    }else {
+    try{
+      dispatch({ type: 'setStatus', payload: 'fetching'})
+      const response = await fetch('https://opentdb.com/api.php?amount=10')
+      // Add Type Annotation for the data
+      const data : QuestionResponse = await(response.json())
+      console.log('data is ',data);
+      if(data.response_code === 0){
+        let question : Question = data.results[0].question
+        dispatch({ type: 'setStatus', payload: 'ready'})
+      }else {
+        dispatch({ type: 'setStatus', payload: 'error'})
+      }
+    }catch(err){
+      console.log('error is ',err);
       dispatch({ type: 'setStatus', payload: 'error'})
     }
+    
     
   }
 
@@ -33,10 +42,16 @@ function App() {
 
   return (
     <>
-      <Score />
-      <Game /> 
-      <h2> {quizState.gameStatus} </h2>
-      <button onClick={() => dispatch({ type: 'setStatus', payload: 'fetching'})}></button>
+      {
+        quizState.gameStatus === 'fetching' ?
+        <FullPageLoader /> : quizState.gameStatus === 'error' ?
+        <p>Error ...</p> : quizState.gameStatus === 'ready' ?
+        <>
+          <Score />
+          <Game /> 
+        </> :
+        ''
+      }
        
     </>
   )
