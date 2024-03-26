60a1e5b2f605b9e191017ae1d803d11b7eb475d4
Brad Stapleton <brad@rethinkcanada.com>
2024-03-26 00:27:52 -0400

    adjust result message

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,10 +1,23 @@
+import { useQuiz  } from '../../context/QuizContext/QuizContext.tsx';
+import { decode } from 'html-entities';
+
 
 function Result() {
 
-
+    const { quizState } = useQuiz();
     return (
         <>  
-            <div className="result correct">&#10003; You answered correctly!</div>
+            {
+                quizState.userAnswer === quizState.question?.correct_answer ? 
+                    <div className="result correct">&#10003; You answered correctly!</div> 
+                :
+                <>
+                    <div className="result incorrect">&#x274c; Your answer was wrong :/</div> <br />
+                    <p>The correct answer is {decode (quizState.question?.correct_answer)}</p>
+                </>
+                
+            }
+            
         </>
     )
 }
